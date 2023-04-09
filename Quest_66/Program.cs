cf311e2940656f2b9b4f18f3adf570dde7b904ac
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-04-09 22:33:02 +0300

    Сумма чисел от N до M

---
+++ Quest_66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в 
+// промежутке от M до N.
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число: ");
+int M = int.Parse(Console.ReadLine()!);
+
+if (M > N) Console.WriteLine(PrintSum(N, M));
+else Console.WriteLine(PrintSum(M, N));
+
+// Метод
+int PrintSum(int start, int end){
+    if (start == end) return start;
+    return (PrintSum(start + 1, end) + start);
+}
