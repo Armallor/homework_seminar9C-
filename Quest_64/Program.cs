426aebced9c9708cc7490e763f37bf55fdc1b9c4
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-04-09 22:31:35 +0300

    Числа от N до 1

---
+++ Quest_64/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке 
+// от N до 1
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(N, 1));
+
+// Метод
+string PrintNumbers(int start, int end){
+    if(start==end) return start.ToString();
+    return (start + " " + PrintNumbers(start - 1, end));
+}
